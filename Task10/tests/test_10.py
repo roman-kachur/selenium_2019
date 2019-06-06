a3fe5369a4fbac83f9ee7b1f3a037ca621580ef1
roman-kachur <roman_kachur@ukr.net>
2019-06-06 19:21:52 +0300

    Add files via upload

--- Task10/tests/test_10.py
+++ Task10/tests/test_10.py
@@ -1,5 +1,5 @@
 
 def test_10(litecart):
-    litecart.campaign_products()
-    litecart.add_items_to_cart()
-    litecart.purge_cart()
+    litecart.campaign_products().add_items_to_cart().purge_cart()
+    #litecart.add_items_to_cart()
+    #litecart.purge_cart()
